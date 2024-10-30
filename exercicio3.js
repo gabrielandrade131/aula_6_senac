64ede16d5c8fec5b294e36d40b531c711ed0827a
gabrielandrade131 <gabrielandrade131@gmail.com>
2024-10-30 20:37:05 -0300

    Update exercicio3.js

--- exercicio3.js
+++ exercicio3.js
@@ -2,7 +2,7 @@ let lado1 = 10
 let lado2 = 10
 let lado3 = 10
 
-if (lado1 == lado2 && lado2 == lado3 && lado1 && lado3) {
+if (lado1 == lado2 && lado2 == lado3) {
     console.log('Equilatero')
 }
 else if (lado1 == lado2 ||  lado2 == lado3 || lado1 == lado3) {
